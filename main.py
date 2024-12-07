a0e62834193abbd9f59f5d76cad045c03a8e3408
ramzy1453 <kemmounramzy93@gmail.com>
2024-12-07 18:38:56 +0100

    add CORS middleware to enable client-side requests

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 
 from app.routers.pdf import router as pdf_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    # allow_credentials=True,
+    allow_origins=["*"],
+    allow_headers=["*"],
+    allow_methods=['GET', 'POST', 'PUT', 'DELETE'],
+)
+
 app.mount("/media", StaticFiles(directory=Path('media')), name="media")
 
 
